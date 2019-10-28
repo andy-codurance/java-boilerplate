34a272b928e3b5b1a8e5a3dd4b7b3f255c2db763
Andrew Pattison <49647893+andy-codurance@users.noreply.github.com>
2019-10-28 10:29:00 +0000

    initial commit

---
+++ src/test/java/Tests.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static com.google.common.truth.Truth.assertThat;
+
+public class Tests {
+
+    @Test
+    public void true_is_true(){
+        assertThat(true).isTrue();
+    }
+}
